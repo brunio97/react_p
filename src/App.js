1737d7a90e99c9157f6096a5916ddde741c608fd
brunio97 <brusalinasv1997@gmail.com>
2024-02-15 09:14:09 -0400

    doctors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/LandingPage';
 import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_UP/Sign_Up';
-
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 function App() {
   return (
     <div className="App">
@@ -15,6 +15,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/sign_up" element={<Sign_Up />} />
+          <Route path="/instant-consultation" element={<InstantConsultation />} />
+         
           {/* Agrega más rutas para otras páginas si es necesario */}
         </Routes>
       </BrowserRouter>

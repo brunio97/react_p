6b297347c9ee95cd421a0c653feb2708a39af375
brunio97 <brusalinasv1997@gmail.com>
2024-02-15 17:56:16 -0400

    doctors3

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:MjA4ODItYnJ1c2Fs@127.0.0.1:27017";
+const mongoURI =  "mongodb://localhost:27017/project";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
-
-export const API_URL = 'https://brusalinasv1-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/your-real-api-endpoint';
-
+export const API_URL = window.location.hostname === "localhost" ? "http://localhost:8181" : "http://localhost:8181";
 console.log(
-    
     "API_URL :",
     API_URL
 );

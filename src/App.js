74871c50673d30a4e7dda80e9c79d11d208ddba6
brunio97 <brusalinasv1997@gmail.com>
2024-02-18 22:17:01 -0400

    Reviews

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-// App.js
 import React from 'react';
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/LandingPage';
@@ -8,8 +6,9 @@ import Login from './Components/Login/Login';
 import Sign_Up from './Components/Sign_UP/Sign_Up';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 import Notification from './Components/Notification/Notification';
-import FindDoctorSearchIC from './Components/InstantConsultation/FindDoctorSearchIC/FindDoctorSearchIC';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
 import BookingConsultation from './Components/BookingConsultation';
+import ReviewForm from './Components/ReviewForm/ReviewForm';
 
 function App() {
   return (
@@ -23,7 +22,9 @@ function App() {
           <Route path="/sign_up" element={<Sign_Up />} />
           <Route path="/instant-consultation" element={<InstantConsultation />} />
           <Route path='/search/doctors' element={<BookingConsultation />} /> 
-          {/* Agrega más rutas para otras páginas si es necesario */}
+          <Route path='/reviews' element={<ReviewForm />} />
+
+          
         </Routes>
       </BrowserRouter>
     </div>
